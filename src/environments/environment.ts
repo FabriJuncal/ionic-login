a662e8ad8fbfaae7cdd66d076cc6ce1d3c1385dc
fjuncal <fabriciojuncal@micam.com.ar>
2022-01-29 13:03:36 -0300

    Ionic Login | Instalación de Firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ionic-login-7fe8b',
+    appId: '1:252867545410:web:c4ba7ed8e00812ba0fe0db',
+    storageBucket: 'ionic-login-7fe8b.appspot.com',
+    apiKey: 'AIzaSyD07vwmrTr5rVC1XYnb7ZSwHcmapLrHmXw',
+    authDomain: 'ionic-login-7fe8b.firebaseapp.com',
+    messagingSenderId: '252867545410',
+    measurementId: 'G-4X7J0WH863',
+  },
   production: false
 };
 
