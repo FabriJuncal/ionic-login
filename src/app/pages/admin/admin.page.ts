46a735a0ad7f0eb5fe139f8f23310f8f1cb0f598
fjuncal <fabriciojuncal@micam.com.ar>
2022-01-30 16:40:50 -0300

    Ionic Login | Desarrollo Admin

---
+++ src/app/pages/admin/admin.page.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-admin',
+  templateUrl: './admin.page.html',
+  styleUrls: ['./admin.page.scss'],
+})
+export class AdminPage implements OnInit {
+
+  constructor(private authSvc: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  async onLogout(){
+    await this.authSvc.logout();
+  }
+
+}
